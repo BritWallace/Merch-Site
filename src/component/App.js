c86a37c15e7ca3c1d8f45d67570e14d1d3f298a3
Brit Wallace <britwallace888@gmail.com>
2022-05-31 19:15:57 -0700

    Co-authored-by: Nathan Conn <nconn34@gmail.com> Co-authored-by: Patrick Dolan <dolanp1992@gmail.com>

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import MerchControl from "./MerchControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <MerchControl />
+    </React.Fragment>
+  );
+}
---
+++ src/component/MerchDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function MerchDetail(props) {
+    const {}merch, onClickingDelete 
+ = props;
+    return (
+        <React.Fragment>
+            <h1>
+    )} 
